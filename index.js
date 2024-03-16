777c353eac7ef0051607b6224373d39bd1049ff3
Veronika Matoušková <andrlovaver@gmail.com>
2024-03-16 16:18:37 +0100

    repaire promennou

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  
  const selectPlan = (planNumber) => {
  let vyber = document.getElementById(`plan${planNumber}`)
-}
+
     let plan1 = document.getElementById('plan1')
     let plan2 = document.getElementById('plan2')
     let plan3 = document.getElementById('plan3')
@@ -19,4 +19,4 @@
         plan1.classList.remove('plan--selected')
         plan2.classList.remove('plan--selected')
         plan3.classList.add('plan--selected')
-    }
+    }}

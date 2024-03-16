54719b13507540514a95b0a00ca5c9a0e339a9a9
Veronika Matoušková <andrlovaver@gmail.com>
2024-03-16 15:37:48 +0100

    repaire js code

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 
  
  const selectPlan = (planNumber) => {
-    let plan1 = document.getElementById('plan1');
-    let plan2 = document.getElementById('plan2');
-    let plan3 = document.getElementById('plan3');
+    let plan1 = document.getElementById('plan1')
+    let plan2 = document.getElementById('plan2')
+    let plan3 = document.getElementById('plan3')
 
     if (planNumber === 1) {
-        plan1.classList.add('plan--selected');
-        plan2.classList.remove('plan--selected');
-        plan3.classList.remove('plan--selected');
+        plan1.classList.add('plan--selected')
+        plan2.classList.remove('plan--selected')
+        plan3.classList.remove('plan--selected')
     } else if (planNumber === 2) {
-        plan1.classList.remove('plan--selected');
-        plan2.classList.add('plan--selected');
-        plan3.classList.remove('plan--selected');
+        plan1.classList.remove('plan--selected')
+        plan2.classList.add('plan--selected')
+        plan3.classList.remove('plan--selected')
     } else if (planNumber === 3) {
-        plan1.classList.remove('plan--selected');
-        plan2.classList.remove('plan--selected');
-        plan3.classList.add('plan--selected');
+        plan1.classList.remove('plan--selected')
+        plan2.classList.remove('plan--selected')
+        plan3.classList.add('plan--selected')
     }
-};
-
-
+}

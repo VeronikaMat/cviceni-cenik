fab087d04fcb5a8a504d7aab400eb31c90b581cc
Veronika Matoušková <andrlovaver@gmail.com>
2024-03-16 17:36:35 +0100

    http://milujipraci.cz/#03

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 
  
  const selectPlan = (planNumber) => {
- 
-    let plan1 = document.getElementById('plan1')
-    let plan2 = document.getElementById('plan2')
-    let plan3 = document.getElementById('plan3')
+ let vyber = document.getElementById(`plan${planNumber}`)
+  
+
+ let plan1 = document.getElementById('plan1')
+ let plan2 = document.getElementById('plan2')
+ let plan3 = document.getElementById('plan3')
+
+    plan1.classList.remove('plan--selected')
+    plan2.classList.remove('plan--selected')
+    plan3.classList.remove('plan--selected')
 
 
 
-
-if (planNumber === 1) {
+        vyber.classList.add('plan1')
+  }
+  
+  /*if (planNumber === 1) {
         plan1.classList.add('plan--selected')
         plan2.classList.remove('plan--selected')
         plan3.classList.remove('plan--selected')
@@ -20,8 +28,4 @@ if (planNumber === 1) {
     } else if (planNumber === 3) {
         plan1.classList.remove('plan--selected')
         plan2.classList.remove('plan--selected')
-        plan3.classList.add('plan--selected')
-        
-    let vyber = document.getElementById(`plan${planNumber}`)
-}
-}
+        plan3.classList.add('plan--selected')*/

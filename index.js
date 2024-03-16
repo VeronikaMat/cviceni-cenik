216564688ab747f81784a4ceef1d058fc9d95ad2
Veronika Matoušková <andrlovaver@gmail.com>
2024-03-16 14:55:20 +0100

    achjo

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
- // všechny plány
+
  
- const selectPlan= (planNumber) => {
-   
-  let plan1 = document.getElementById ('plan1')
-  let plan2 = document.getElementById ('plan2')
-  let plan3 = document.getElementById ('plan3')
+ const selectPlan = (planNumber) => {
+    let plan1 = document.getElementById('plan1');
+    let plan2 = document.getElementById('plan2');
+    let plan3 = document.getElementById('plan3');
 
-
-    // Projde to všechny plány
-    for (let i = 0; i < plans.length; i++) {
-    // Odebrání třídy od všech plánů
-        plans[i].classList.remove('plan--selected')
+    if (planNumber === 1) {
+        plan1.classList.add('plan--selected');
+        plan2.classList.remove('plan--selected');
+        plan3.classList.remove('plan--selected');
+    } else if (planNumber === 2) {
+        plan1.classList.remove('plan--selected');
+        plan2.classList.add('plan--selected');
+        plan3.classList.remove('plan--selected');
+    } else if (planNumber === 3) {
+        plan1.classList.remove('plan--selected');
+        plan2.classList.remove('plan--selected');
+        plan3.classList.add('plan--selected');
     }
+};
 
-    // Přidání třídy k  plánu
-    let selectedPlan = document.querySelector('.plan-' + planNumber)
-    if (selectedPlan) {
-        selectedPlan.classList.add('plan--selected')
-    }
-}
+
+ 

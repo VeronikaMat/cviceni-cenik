1b0561d3a99333b3faa07157c59f60135883b5a9
Veronika Matoušková <andrlovaver@gmail.com>
2024-03-16 18:19:23 +0100

    repare js code

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 
- 
  const selectPlan = (planNumber) => {
  let vyber = document.getElementById(`plan${planNumber}`)
-  
+ 
+ 
 
  let plan1 = document.getElementById('plan1')
  let plan2 = document.getElementById('plan2')
  let plan3 = document.getElementById('plan3')
 
-    plan1.classList.remove('plan--selected')
-    plan2.classList.remove('plan--selected')
-    plan3.classList.remove('plan--selected')
-
-
-
+if (planNumber === 1) {
         vyber.classList.add('plan--selected')
-  }
-  
-  /*if (planNumber === 1) {
-        plan1.classList.add('plan--selected')
         plan2.classList.remove('plan--selected')
         plan3.classList.remove('plan--selected')
     } else if (planNumber === 2) {
         plan1.classList.remove('plan--selected')
-        plan2.classList.add('plan--selected')
+        vyber.classList.add('plan--selected')
         plan3.classList.remove('plan--selected')
     } else if (planNumber === 3) {
         plan1.classList.remove('plan--selected')
         plan2.classList.remove('plan--selected')
-        plan3.classList.add('plan--selected')*/
+        vyber.classList.add('plan--selected')
+
+        
+  }
+
+ 
+
+}
+selectPlan(1)
+  /**/
